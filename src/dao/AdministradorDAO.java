bdace59ae07d0cba7ffb045b2401fb67a1c6350e
Pedro Vinícius <pedrovinilira2005@gmail.com>
2025-01-16 14:20:12 -0300

    simplificação

--- src/dao/AdministradorDAO.java
+++ src/dao/AdministradorDAO.java
@@ -8,16 +8,17 @@ import java.sql.SQLException;
 
 public class AdministradorDAO {
     public void cadastrarAdministrador(Administrador admin) {
-        String sql = "INSERT INTO ADMINISTRADOR (LOGIN_USUARIO, CARGO) VALUES (?,?)";
+        String sql = "INSERT INTO ADMINISTRADOR (LOGIN_USUARIO, CARGO) VALUES (?,?)"; // aqui ele prepara para fazer o insert, colocando "?" como placeholder
+        // que em breve sera trocado no try
         PreparedStatement ps = null;
 
         try{
-            ps = ConexaoDB.getConexao().prepareStatement(sql);
-            ps.setString(1, admin.getLogin_usuario());
-            ps.setString(2, admin.getCargo());
+            ps = ConexaoDB.getConexao().prepareStatement(sql); // aqui realiza a conexao e prepara para executar a consulta SQL
+            ps.setString(1, admin.getLogin_usuario()); // aqui seta o primeiro parametro como login
+            ps.setString(2, admin.getCargo()); // segundo parametro como cargo
 
-            ps.execute();
-            ps.close();
+            ps.execute(); // executa
+            ps.close(); // encerra
 
         } catch (SQLException e) {
             e.printStackTrace();
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,4 +1,55 @@
 package view;
 
-public class Main {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+    private JLabel jlNameCadastro;
+    private JTextField jfNameCadastro;
+    private JButton jbCadastro;
+    private JPanel MainPanel;
+    private JLabel jlCPFCadastro;
+    private JTextField jfCPFCadastro;
+    private JTextField jfUserNameCadastro;
+    private JLabel jlSenhaCadastro;
+    private JLabel jlUserNameCadastro;
+    private JPasswordField jfPasswordCadastro;
+    private JPanel SidePanel;
+    private JLabel jlCadastrese;
+    private JLabel jlLogin;
+    private JTextField jfUsernameLogin;
+    private JPasswordField jfPasswordLogin;
+    private JLabel jlUsernameLogin;
+    private JLabel jlPasswordLogin;
+    private JComboBox jcDayCadastro;
+    private JComboBox jcMonthCadastro;
+    private JComboBox jcYearCadastro;
+    private JButton jbLogin;
+    private JLabel jlBirthDay;
+
+    public Main() {
+        ImageIcon img = new ImageIcon("C:\\Users\\Pedro Lira\\Documents\\projetos\\projetoPoo\\icon\\user.png");
+        setIconImage(img.getImage());
+        setContentPane(MainPanel);
+        setTitle("Cadastro");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1024, 720);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
+        jbCadastro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(Main.this, "Your first name is " + jfNameCadastro.getText());
+                jfNameCadastro.setText("");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
 }
